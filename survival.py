1df30ccdb9266ac1a27a97361480f70b9ff11f5b
Matthew O'Brien <matthewobrien@matthews-air.lan>
2021-03-22 19:38:35 -0500

    enh: may move to a Jupyter lab environemtn or connect VScode. I'm not terribly happy with the Spyder distribution.

--- survival.py
+++ survival.py
@@ -9,20 +9,22 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 data = sm.datasets.get_rdataset("flchain", "survival").data
 
-data.head()
-data.loc[data.death != 1, :].head()
-data.loc[data.death == 1, :].head()
-df = data.loc[data.sex == "F", :]
-mod = sm.SurvfuncRight(df["futime"], df["death"])
-
-mod.summary()
 
 
-fig = mod.plot()
-lcb, ucb = mod.simultaneous_cb()
-ax = fig.get_axes()[0]
-ax.fill_between(mod.surv_times, lcb, ucb, color='lightgrey')
-ax.set_xlim(365, 365*10)
-ax.set_ylim(0.7, 1)
-ax.set_ylabel("Proportion alive")
-ax.set_xlabel("Days since enrollment")
+if __name__ == "__main__":
+    data.head()
+    data.loc[data.death != 1, :].head()
+    data.loc[data.death == 1, :].head()
+    df = data.loc[data.sex == "F", :]
+    mod = sm.SurvfuncRight(df["futime"], df["death"])
+
+    mod.summary()
+
+    fig = mod.plot()
+    lcb, ucb = mod.simultaneous_cb()
+    ax = fig.get_axes()[0]
+    ax.fill_between(mod.surv_times, lcb, ucb, color='lightgrey')
+    ax.set_xlim(365, 365 * 10)
+    ax.set_ylim(0.7, 1)
+    ax.set_ylabel("Proportion alive")
+    ax.set_xlabel("Days since enrollment")
