8bc74c8b1ab7bfd5e5fdf784c44ff6f87ec9f16c
Matthew O'Brien <maobrien11235@gmail.com>
2021-03-28 20:59:14 -0500

    ENH: gonna go with absolute imports for the time being because relative imports were failing for an odd reason. Want to move on with more of the meatier parts of the project.

--- src/main.py
+++ src/main.py
@@ -8,11 +8,8 @@ Created on Sat Mar 27 21:36:20 2021
 @purpose: File for main modeling and execution of the surivival
 analysis.
 """
-import ANTNM_Survival_Analysis as antnm
-dir(antnm.src.dat_eng.import_data)
-
-# Project Imports
-from antnm.src.dat_eng import (
+# todo: research why my relative imports were failing.
+from ANTNM_Survival_Analysis.src.dat_eng import (
     import_data
     )
 
@@ -20,4 +17,5 @@ from antnm.src.dat_eng import (
 test = import_data("/Users/matthewobrien/Documents/Learning/ANTNM_Survival_analysis/",
             "Manual_data.csv")
 test.info()
+
 
